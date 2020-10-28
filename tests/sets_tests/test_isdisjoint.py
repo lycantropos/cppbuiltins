2fc4fef26238514b018323e0e295394e01d91ef6
Azat Ibrakov <azatibrakov@gmail.com>
2020-10-28 15:49:22 +0300

    Add test for native & alternative 'set.isdisjoint' methods

---
+++ tests/sets_tests/test_isdisjoint.py
@@ -0,0 +1,15 @@
+from typing import (Any,
+                    List)
+
+from hypothesis import given
+
+from tests.utils import (AlternativeNativeSetsPair,
+                         equivalence)
+from . import strategies
+
+
+@given(strategies.sets_pairs, strategies.objects_lists)
+def test_basic(pair: AlternativeNativeSetsPair, other: List[Any]) -> None:
+    alternative, native = pair
+
+    assert equivalence(alternative.isdisjoint(other), native.isdisjoint(other))
