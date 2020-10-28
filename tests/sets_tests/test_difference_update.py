1a99e0f0b67a56e48c5fa8981565f56dddf5530a
Azat Ibrakov <azatibrakov@gmail.com>
2020-10-28 14:25:15 +0300

    Add test for native & alternative 'set.difference_update' methods

---
+++ tests/sets_tests/test_difference_update.py
@@ -0,0 +1,21 @@
+from typing import (Any,
+                    List)
+
+from hypothesis import given
+
+from tests.utils import (AlternativeNativeSetsPair,
+                         are_alternative_native_sets_equal)
+from . import strategies
+
+
+@given(strategies.sets_pairs, strategies.objects_lists_lists)
+def test_basic(pair: AlternativeNativeSetsPair,
+               others: List[List[Any]]) -> None:
+    alternative, native = pair
+
+    alternative_result, native_result = (
+        alternative.difference_update(*others),
+        native.difference_update(*others))
+
+    assert alternative_result is native_result is None
+    assert are_alternative_native_sets_equal(alternative, native)
