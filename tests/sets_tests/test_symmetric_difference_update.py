12ffb7c5b72b62d3d07aedc0a8576640e304f199
Azat Ibrakov <azatibrakov@gmail.com>
2020-10-28 14:38:55 +0300

    Complete test for native & alternative 'set.symmetric_difference_update' methods

--- tests/sets_tests/test_symmetric_difference_update.py
+++ tests/sets_tests/test_symmetric_difference_update.py
@@ -8,14 +8,13 @@ from tests.utils import (AlternativeNativeSetsPair,
 from . import strategies
 
 
-@given(strategies.sets_pairs, strategies.objects_lists_lists)
-def test_basic(pair: AlternativeNativeSetsPair,
-               others: List[List[Any]]) -> None:
+@given(strategies.sets_pairs, strategies.objects_lists)
+def test_basic(pair: AlternativeNativeSetsPair, other: List[Any]) -> None:
     alternative, native = pair
 
     alternative_result, native_result = (
-        alternative.symmetric_difference_update(*others),
-        native.symmetric_difference_update(*others))
+        alternative.symmetric_difference_update(other),
+        native.symmetric_difference_update(other))
 
     assert alternative_result is native_result is None
     assert are_alternative_native_sets_equal(alternative, native)
