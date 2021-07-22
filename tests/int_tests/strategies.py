158e4f49e50acc4fbe2c3e35fdcb071c09671416
Azat Ibrakov <azatibrakov@gmail.com>
2021-07-22 18:06:28 +0300

    Complete strategies

--- tests/int_tests/strategies.py
+++ tests/int_tests/strategies.py
@@ -1,4 +1,5 @@
 import re
+import string
 
 from hypothesis import strategies
 
@@ -7,15 +8,6 @@ from tests.utils import AlternativeInt
 whitespaces_class = r'[\f\n\r\t\v ]'
 decimal_int_strings = strategies.from_regex(
         re.compile(r'\A{whitespaces}*\d(_?\d+)*{whitespaces}*\Z'
-                   .format(whitespaces=whitespaces_class)))
-binary_int_strings = strategies.from_regex(
-        re.compile(r'\A{whitespaces}*[0-1](_?[0-1]+)*{whitespaces}*\Z'
-                   .format(whitespaces=whitespaces_class)))
-octal_int_strings = strategies.from_regex(
-        re.compile(r'\A{whitespaces}*[0-7](_?[0-7]+)*{whitespaces}*\Z'
-                   .format(whitespaces=whitespaces_class)))
-hexadecimal_int_strings = strategies.from_regex(
-        re.compile(r'\A{whitespaces}*[\da-f](_?[\da-f]+)*{whitespaces}*\Z'
                    .format(whitespaces=whitespaces_class)))
 prefixed_binary_int_strings = strategies.from_regex(
         re.compile(r'\A{whitespaces}*0b(_?[0-1]+)+{whitespaces}*\Z'
@@ -30,12 +22,29 @@ prefixed_hexadecimal_int_strings = strategies.from_regex(
 ints = (strategies.builds(AlternativeInt, decimal_int_strings)
         | strategies.builds(AlternativeInt, decimal_int_strings,
                             strategies.sampled_from([0, 10]))
-        | strategies.builds(AlternativeInt, binary_int_strings,
-                            strategies.just(2))
-        | strategies.builds(AlternativeInt, hexadecimal_int_strings,
-                            strategies.just(16))
-        | strategies.builds(AlternativeInt, octal_int_strings,
-                            strategies.just(8))
+        | strategies.one_of([strategies.builds(
+                AlternativeInt,
+                strategies.from_regex(
+                        re.compile(r'\A{whitespaces}*{digits}'
+                                   r'(_?{digits}+)*{whitespaces}*\Z'
+                                   .format(digits='[0-{}]'.format(max_digit),
+                                           whitespaces=whitespaces_class))),
+                strategies.just(max_digit + 1))
+            for max_digit in range(1, 10)])
+        | strategies.one_of([strategies.builds(
+                AlternativeInt,
+                strategies.from_regex(
+                        re.compile(r'\A{whitespaces}*{digits}'
+                                   r'(_?{digits}+)*{whitespaces}*\Z'
+                                   .format(digits=('[0-9a-{max_lower}'
+                                                   'A-{max_upper}]'
+                                                   .format(max_lower=max_lower,
+                                                           max_upper=
+                                                           max_lower.upper())),
+                                           whitespaces=whitespaces_class))),
+                strategies.just(base))
+            for base, max_lower in enumerate(string.ascii_lowercase,
+                                             start=11)])
         | strategies.builds(AlternativeInt, prefixed_binary_int_strings,
                             strategies.sampled_from([0, 2]))
         | strategies.builds(AlternativeInt, prefixed_hexadecimal_int_strings,
