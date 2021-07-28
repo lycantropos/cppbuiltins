b7b5d25635720447110165dc78e15a87de1fc8d4
Azat Ibrakov <azatibrakov@gmail.com>
2021-07-28 12:00:50 +0300

    Complete tests configurations

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,9 @@
 from hypothesis import (HealthCheck,
+                        Verbosity,
                         settings)
 
 settings.register_profile('default',
                           deadline=None,
                           suppress_health_check=[HealthCheck.filter_too_much,
-                                                 HealthCheck.too_slow])
+                                                 HealthCheck.too_slow],
+                          verbosity=Verbosity.verbose)
