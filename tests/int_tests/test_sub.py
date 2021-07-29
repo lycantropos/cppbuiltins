42262f4e7f90c8640160b4c7af38ec13fe81517c
Azat Ibrakov <azatibrakov@gmail.com>
2021-07-29 14:53:37 +0300

    Complete test of 'int.__sub__' magic method

--- tests/int_tests/test_sub.py
+++ tests/int_tests/test_sub.py
@@ -25,6 +25,11 @@ def test_commutative_case(first: AlternativeInt,
     assert equivalence(first - second == second - first, first == second)
 
 
+@given(strategies.ints, strategies.ints)
+def test_sign(first: AlternativeInt, second: AlternativeInt) -> None:
+    assert first - second >= 0 if first >= second else first - second < 0
+
+
 @given(strategies.ints, strategies.zero_ints)
 def test_right_neutral_element(first: AlternativeInt,
                                second: AlternativeInt) -> None:
