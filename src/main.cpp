6627086463db7414d8f36a93f2d6d9936bfc427c
Azat Ibrakov <azatibrakov@gmail.com>
2021-07-30 16:07:04 +0300

    Complete 'int.__deepcopy__' magic method

--- src/main.cpp
+++ src/main.cpp
@@ -1061,7 +1061,8 @@ PYBIND11_MODULE(MODULE_NAME, m) {
       .def("__hash__", &Int::hash)
       .def("__bool__", &Int::operator bool)
       .def("__copy__", [](const Int& self) -> const Int& { return self; })
-      .def("__deepcopy__", [](const Int& self) -> Int { return self; })
+      .def("__deepcopy__",
+           [](const Int& self, const py::dict&) -> Int { return self; })
       .def("__int__", &Int::to_state)
       .def("__float__", &Int::operator double)
       .def("__repr__", &to_repr<Int>)
