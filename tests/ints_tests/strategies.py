6c5635db643b10ea7b24199705f6ff66aa5c27a2
Azat Ibrakov <azatibrakov@gmail.com>
2021-08-02 21:27:39 +0300

    Complete formatting

--- tests/ints_tests/strategies.py
+++ tests/ints_tests/strategies.py
@@ -2,7 +2,8 @@ from hypothesis import strategies
 
 from tests.strategies import (decimal_int_strings_with_leading_zeros,
                               int_strings_with_bases)
-from tests.utils import pack, to_alternative_native_ints_pair
+from tests.utils import (pack,
+                         to_alternative_native_ints_pair)
 
 ints_pairs = (strategies.builds(to_alternative_native_ints_pair,
                                 decimal_int_strings_with_leading_zeros)
