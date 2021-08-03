5f28a4247b862b803f409ae27aba863046ae3574
Azat Ibrakov <azatibrakov@gmail.com>
2021-08-03 16:48:23 +0300

    Complete test for native & alternative 'int.__divmod__' magic methods

--- tests/ints_tests/strategies.py
+++ tests/ints_tests/strategies.py
@@ -1,5 +1,3 @@
-from operator import itemgetter
-
 from hypothesis import strategies
 
 from tests.strategies import (decimal_int_strings_with_leading_zeros,
@@ -11,4 +9,3 @@ ints_pairs = (strategies.builds(to_alternative_native_ints_pair,
                                 decimal_int_strings_with_leading_zeros)
               | strategies.builds(pack(to_alternative_native_ints_pair),
                                   int_strings_with_bases))
-non_zero_ints_pairs = ints_pairs.filter(itemgetter(0))
--- tests/ints_tests/test_divmod.py
+++ tests/ints_tests/test_divmod.py
@@ -1,3 +1,4 @@
+import pytest
 from hypothesis import given
 
 from tests.utils import (AlternativeNativeIntsPair,
@@ -5,17 +6,23 @@ from tests.utils import (AlternativeNativeIntsPair,
 from . import strategies
 
 
-@given(strategies.ints_pairs, strategies.non_zero_ints_pairs)
+@given(strategies.ints_pairs, strategies.ints_pairs)
 def test_basic(dividends_pair: AlternativeNativeIntsPair,
                divisors_pair: AlternativeNativeIntsPair) -> None:
     alternative_dividend, native_dividend = dividends_pair
     alternative_divisor, native_divisor = divisors_pair
 
-    alternative_quotient, alternative_remainder = divmod(alternative_dividend,
-                                                         alternative_divisor)
-    native_quotient, native_remainder = divmod(native_dividend, native_divisor)
+    try:
+        alternative_quotient, alternative_remainder = divmod(
+                alternative_dividend, alternative_divisor)
+    except ZeroDivisionError:
+        with pytest.raises(ZeroDivisionError):
+            divmod(native_dividend, native_divisor)
+    else:
+        native_quotient, native_remainder = divmod(native_dividend,
+                                                   native_divisor)
 
-    assert are_alternative_native_ints_equal(alternative_quotient,
-                                             native_quotient)
-    assert are_alternative_native_ints_equal(alternative_remainder,
-                                             native_remainder)
+        assert are_alternative_native_ints_equal(alternative_quotient,
+                                                 native_quotient)
+        assert are_alternative_native_ints_equal(alternative_remainder,
+                                                 native_remainder)
