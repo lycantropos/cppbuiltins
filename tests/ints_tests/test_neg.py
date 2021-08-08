3a5d098f9bfcd93e98f78c427b758c9032ed8db5
Azat Ibrakov <azatibrakov@gmail.com>
2021-08-08 12:14:39 +0300

    Add test for native & alternative 'int.__neg__' magic methods

---
+++ tests/ints_tests/test_neg.py
@@ -0,0 +1,12 @@
+from hypothesis import given
+
+from tests.utils import (AlternativeNativeIntsPair,
+                         are_alternative_native_ints_equal)
+from . import strategies
+
+
+@given(strategies.ints_pairs)
+def test_basic(pair: AlternativeNativeIntsPair) -> None:
+    alternative, native = pair
+
+    assert are_alternative_native_ints_equal(-alternative, -native)
