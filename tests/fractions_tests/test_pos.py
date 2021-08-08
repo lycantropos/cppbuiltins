dfdc13be17c1081196f24879667156d6cac29cff
Azat Ibrakov <azatibrakov@gmail.com>
2021-08-08 13:12:29 +0300

    Add test for native & alternative 'Fraction.__pos__' magic methods

---
+++ tests/fractions_tests/test_pos.py
@@ -0,0 +1,12 @@
+from hypothesis import given
+
+from tests.utils import (AlternativeNativeFractionsPair,
+                         are_alternative_native_fractions_equal)
+from . import strategies
+
+
+@given(strategies.fractions_pairs)
+def test_basic(pair: AlternativeNativeFractionsPair) -> None:
+    alternative, native = pair
+
+    assert are_alternative_native_fractions_equal(+alternative, +native)
