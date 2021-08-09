a0a4d747ace8218787773d7a5adf2cef28e6fe02
Azat Ibrakov <azatibrakov@gmail.com>
2021-08-09 12:59:40 +0300

    Add test for native & alternative 'Fraction.__eq__' magic methods

---
+++ tests/fractions_tests/test_equals.py
@@ -0,0 +1,15 @@
+from hypothesis import given
+
+from tests.utils import (AlternativeNativeFractionsPair,
+                         equivalence)
+from . import strategies
+
+
+@given(strategies.fractions_pairs, strategies.fractions_pairs)
+def test_basic(first_pair: AlternativeNativeFractionsPair,
+               second_pair: AlternativeNativeFractionsPair) -> None:
+    alternative_first, native_first = first_pair
+    alternative_second, native_second = second_pair
+
+    assert equivalence(alternative_first == alternative_second,
+                       native_first == native_second)
