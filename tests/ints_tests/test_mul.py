420fc8068da9dd5f1f1354722965db145770b8d2
Azat Ibrakov <azatibrakov@gmail.com>
2021-08-10 12:49:21 +0300

    Add test for native & alternative 'int.__mul__' magic methods

---
+++ tests/ints_tests/test_mul.py
@@ -0,0 +1,17 @@
+from hypothesis import given
+
+from tests.utils import (AlternativeNativeIntsPair,
+                         are_alternative_native_ints_equal)
+from . import strategies
+
+
+@given(strategies.ints_pairs, strategies.ints_pairs)
+def test_basic(first_pair: AlternativeNativeIntsPair,
+               second_pair: AlternativeNativeIntsPair) -> None:
+    alternative_first, native_first = first_pair
+    alternative_second, native_second = second_pair
+
+    alternative_result = alternative_first * alternative_second
+    native_result = native_first * native_second
+
+    assert are_alternative_native_ints_equal(alternative_result, native_result)
