b0999859515ce170a554864242cbf0fdfade1e73
Azat Ibrakov <azatibrakov@gmail.com>
2021-08-10 16:55:02 +0300

    Complete strategies

--- tests/ints_tests/strategies.py
+++ tests/ints_tests/strategies.py
@@ -1,5 +1,9 @@
 from hypothesis import strategies
 
+from tests.strategies import (decimal_int_strings_with_leading_zeros,
+                              int_strings_with_bases)
 from tests.utils import to_alternative_native_ints_pair
 
+decimal_int_strings_with_leading_zeros = decimal_int_strings_with_leading_zeros
+int_strings_with_bases = int_strings_with_bases
 ints_pairs = strategies.integers().map(to_alternative_native_ints_pair)
