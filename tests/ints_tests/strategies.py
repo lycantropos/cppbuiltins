30ece3d4e90fe4989ca8c6de1b6b406c3a5ad803
Azat Ibrakov <azatibrakov@gmail.com>
2021-08-11 01:52:01 +0300

    Add test for native & alternative 'int.__pow__' magic methods

--- tests/ints_tests/strategies.py
+++ tests/ints_tests/strategies.py
@@ -7,3 +7,12 @@ from tests.utils import to_alternative_native_ints_pair
 decimal_int_strings_with_leading_zeros = decimal_int_strings_with_leading_zeros
 int_strings_with_bases = int_strings_with_bases
 ints_pairs = strategies.integers().map(to_alternative_native_ints_pair)
+non_zero_ints_pairs = (strategies.integers().filter(bool)
+                       .map(to_alternative_native_ints_pair))
+exponents_with_moduli_pairs = (
+        strategies.tuples(strategies.integers(0, 100)
+                          .map(to_alternative_native_ints_pair),
+                          strategies.tuples(strategies.none(),
+                                            strategies.none())
+                          | non_zero_ints_pairs)
+        | strategies.tuples(ints_pairs, non_zero_ints_pairs))
---
+++ tests/ints_tests/test_pow.py
@@ -0,0 +1,33 @@
+from typing import (Tuple,
+                    Union)
+
+import pytest
+from hypothesis import given
+
+from tests.utils import (AlternativeNativeIntsPair,
+                         are_alternative_native_ints_equal)
+from . import strategies
+
+
+@given(strategies.ints_pairs, strategies.exponents_with_moduli_pairs)
+def test_basic(bases_pair: AlternativeNativeIntsPair,
+               exponents_pair_with_moduli_pair
+               : Tuple[AlternativeNativeIntsPair,
+                       Union[Tuple[None, None], AlternativeNativeIntsPair]]
+               ) -> None:
+    alternative_base, native_base = bases_pair
+    exponents_pair, moduli_pair = exponents_pair_with_moduli_pair
+    alternative_exponent, native_exponent = exponents_pair
+    alternative_modulus, native_modulus = moduli_pair
+
+    try:
+        alternative_result = pow(alternative_base, alternative_exponent,
+                                 alternative_modulus)
+    except ValueError:
+        with pytest.raises(ValueError):
+            pow(native_base, native_exponent, native_modulus)
+    else:
+        native_result = pow(native_base, native_exponent, native_modulus)
+
+        assert are_alternative_native_ints_equal(alternative_result,
+                                                 native_result)
