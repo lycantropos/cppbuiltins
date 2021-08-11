01c32ef7b387c99f80b5fe86f1bd3a8def6db468
Azat Ibrakov <azatibrakov@gmail.com>
2021-08-11 18:23:26 +0300

    Complete tests configurations

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,5 +6,4 @@ settings.register_profile('default',
                           deadline=None,
                           suppress_health_check=[HealthCheck.filter_too_much,
                                                  HealthCheck.too_slow],
-                          max_examples=10 ** 4,
                           verbosity=Verbosity.verbose)
