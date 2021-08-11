dc592e77992c3f6f2364503a2d8ae6e8b77559ad
Azat Ibrakov <azatibrakov@gmail.com>
2021-08-11 19:03:46 +0300

    Complete test of native & alternative 'Fraction.__floordiv__' magic methods

--- tests/fractions_tests/test_floordiv.py
+++ tests/fractions_tests/test_floordiv.py
@@ -1,14 +1,18 @@
+from typing import Union
+
 import pytest
 from hypothesis import given
 
 from tests.utils import (AlternativeNativeFractionsPair,
+                         AlternativeNativeIntsPair,
                          are_alternative_native_ints_equal)
 from . import strategies
 
 
-@given(strategies.fractions_pairs, strategies.fractions_pairs)
+@given(strategies.fractions_pairs, strategies.fractions_or_ints_pairs)
 def test_basic(dividends_pair: AlternativeNativeFractionsPair,
-               divisors_pair: AlternativeNativeFractionsPair) -> None:
+               divisors_pair: Union[AlternativeNativeFractionsPair,
+                                    AlternativeNativeIntsPair]) -> None:
     alternative_dividend, native_dividend = dividends_pair
     alternative_divisor, native_divisor = divisors_pair
 
