5dc435361bbb4926cd9813a68fe1e71f8ec641a5
Azat Ibrakov <azatibrakov@gmail.com>
2021-08-11 18:52:10 +0300

    Add test for native & alternative 'Fraction.__rmod__' magic methods

---
+++ tests/fractions_tests/test_rmod.py
@@ -0,0 +1,25 @@
+import pytest
+from hypothesis import given
+
+from tests.utils import (AlternativeNativeFractionsPair,
+                         AlternativeNativeIntsPair,
+                         are_alternative_native_fractions_equal)
+from . import strategies
+
+
+@given(strategies.ints_pairs, strategies.fractions_pairs)
+def test_basic(dividends_pair: AlternativeNativeIntsPair,
+               divisors_pair: AlternativeNativeFractionsPair) -> None:
+    alternative_dividend, native_dividend = dividends_pair
+    alternative_divisor, native_divisor = divisors_pair
+
+    try:
+        alternative_result = alternative_dividend % alternative_divisor
+    except ZeroDivisionError:
+        with pytest.raises(ZeroDivisionError):
+            native_dividend % native_divisor
+    else:
+        native_result = native_dividend % native_divisor
+
+        assert are_alternative_native_fractions_equal(alternative_result,
+                                                      native_result)
