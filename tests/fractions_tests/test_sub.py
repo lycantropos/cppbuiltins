14b2aeb54509d929f30a5f40797407fe8e0c1c93
Azat Ibrakov <azatibrakov@gmail.com>
2021-08-11 18:42:16 +0300

    Complete test of native & alternative 'Fraction.__sub__' magic methods

--- tests/fractions_tests/test_sub.py
+++ tests/fractions_tests/test_sub.py
@@ -1,18 +1,22 @@
+from typing import Union
+
 from hypothesis import given
 
 from tests.utils import (AlternativeNativeFractionsPair,
+                         AlternativeNativeIntsPair,
                          are_alternative_native_fractions_equal)
 from . import strategies
 
 
-@given(strategies.fractions_pairs, strategies.fractions_pairs)
-def test_basic(minuends_pair: AlternativeNativeFractionsPair,
-               subtrahends_pair: AlternativeNativeFractionsPair) -> None:
-    alternative_minuend, native_minuend = minuends_pair
-    alternative_subtrahend, native_subtrahend = subtrahends_pair
+@given(strategies.fractions_pairs, strategies.fractions_or_ints_pairs)
+def test_basic(first_pair: AlternativeNativeFractionsPair,
+               second_pair: Union[AlternativeNativeFractionsPair,
+                                  AlternativeNativeIntsPair]) -> None:
+    alternative_first, native_first = first_pair
+    alternative_second, native_second = second_pair
 
-    alternative_result = alternative_minuend - alternative_subtrahend
-    native_result = native_minuend - native_subtrahend
+    alternative_result = alternative_first - alternative_second
+    native_result = native_first - native_second
 
     assert are_alternative_native_fractions_equal(alternative_result,
                                                   native_result)
