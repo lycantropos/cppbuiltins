7d02debe23288966c405f6571777e485a3577782
Azat Ibrakov <azatibrakov@gmail.com>
2021-08-15 14:13:45 +0300

    Add test for native & alternative 'int.__int__' magic methods

---
+++ tests/ints_tests/test_int.py
@@ -0,0 +1,11 @@
+from hypothesis import given
+
+from tests.utils import AlternativeNativeIntsPair
+from . import strategies
+
+
+@given(strategies.ints_pairs)
+def test_basic(pair: AlternativeNativeIntsPair) -> None:
+    alternative, native = pair
+
+    assert int(alternative) == int(native)
