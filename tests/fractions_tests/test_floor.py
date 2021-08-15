19aa344b9da74e03cc8801bb4970e0ebe829737d
Azat Ibrakov <azatibrakov@gmail.com>
2021-08-15 13:58:02 +0300

    Add test for native & alternative 'Fraction.__floor__' magic methods

---
+++ tests/fractions_tests/test_floor.py
@@ -0,0 +1,15 @@
+import math
+
+from hypothesis import given
+
+from tests.utils import (AlternativeNativeFractionsPair,
+                         are_alternative_native_ints_equal)
+from . import strategies
+
+
+@given(strategies.fractions_pairs)
+def test_basic(pair: AlternativeNativeFractionsPair) -> None:
+    alternative, native = pair
+
+    assert are_alternative_native_ints_equal(math.floor(alternative),
+                                             math.floor(native))
