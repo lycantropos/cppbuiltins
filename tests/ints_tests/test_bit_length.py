d1b15b2dc5a70bcdc8645cd471463b3f71f874a6
Azat Ibrakov <azatibrakov@gmail.com>
2021-10-18 21:23:46 +0300

    Add test for native & alternative 'int.bit_length' method

---
+++ tests/ints_tests/test_bit_length.py
@@ -0,0 +1,13 @@
+from hypothesis import given
+
+from tests.utils import (AlternativeNativeIntsPair,
+                         are_alternative_native_ints_equal)
+from . import strategies
+
+
+@given(strategies.ints_pairs)
+def test_basic(pair: AlternativeNativeIntsPair) -> None:
+    alternative, native = pair
+
+    assert are_alternative_native_ints_equal(alternative.bit_length(),
+                                             native.bit_length())
