e8b1d20e72f076fdf3aba8c920764673852c0a50
Azat Ibrakov <azatibrakov@gmail.com>
2021-10-21 21:21:35 +0300

    Add test for native & alternative 'int.__rshift__' magic methods

---
+++ tests/ints_tests/test_rshift.py
@@ -0,0 +1,24 @@
+import pytest
+from hypothesis import given
+
+from tests.utils import (AlternativeNativeIntsPair,
+                         are_alternative_native_ints_equal)
+from . import strategies
+
+
+@given(strategies.ints_pairs, strategies.single_byte_ints_pairs)
+def test_basic(dividends_pair: AlternativeNativeIntsPair,
+               divisors_pair: AlternativeNativeIntsPair) -> None:
+    alternative_dividend, native_dividend = dividends_pair
+    alternative_divisor, native_divisor = divisors_pair
+
+    try:
+        alternative_result = alternative_dividend >> alternative_divisor
+    except ValueError:
+        with pytest.raises(ValueError):
+            native_dividend >> native_divisor
+    else:
+        native_result = native_dividend >> native_divisor
+
+        assert are_alternative_native_ints_equal(alternative_result,
+                                                 native_result)
